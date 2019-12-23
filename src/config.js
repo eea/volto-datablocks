dba74c5a9cb082d0d92748f7822c2521afdbf574
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-12-23 07:40:47 +0200

    Added WIP dataconnected block

--- src/config.js
+++ src/config.js
@@ -1,5 +1,10 @@
 import * as addonReducers from './reducers';
+
 import DataQueryWidget from './DataQueryWidget/Widget';
+import DataBlockView from './DataConnectedBlock/View';
+import DataBlockEdit from './DataConnectedBlock/Edit';
+
+import chartIcon from '@plone/volto/icons/world.svg';
 
 export function applyConfig(config) {
   config.widgets.id.data_query = DataQueryWidget;
@@ -8,5 +13,15 @@ export function applyConfig(config) {
     ...config.addonReducers,
     ...addonReducers,
   };
+
+  config.blocks.blocksConfig.data_connected_block = {
+    id: 'data_connected_block',
+    title: 'Simple Data Connected Block',
+    view: DataBlockView,
+    edit: DataBlockEdit,
+    icon: chartIcon,
+    group: 'custom_addons',
+  };
+
   return config;
 }
