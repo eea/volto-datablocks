c42914eb53f4f0712c4fd2339aea645238f9d345
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-12-24 20:35:38 +0200

    Make table fit view

--- src/DataConnector/View.jsx
+++ src/DataConnector/View.jsx
@@ -25,28 +25,30 @@ export class DataConnectorView extends Component {
       <div className="data-connector-view">
         <h2>{content.title}</h2>
         <pre>{content.sql_query}</pre>
-        {provider_data && (
-          <Table compact striped>
-            <Table.Header>
-              {Object.keys(provider_data).map(k => (
-                <Table.HeaderCell key={k}>{k}</Table.HeaderCell>
-              ))}
-            </Table.Header>
-            <Table.Body>
-              {Array(row_size)
-                .fill()
-                .map((_, i) => (
-                  <Table.Row>
-                    {Object.keys(provider_data).map(k => (
-                      <Table.Cell key={`${i}-${k}`}>
-                        {provider_data[k][i]}
-                      </Table.Cell>
-                    ))}
-                  </Table.Row>
+        <div style={{ overflow: 'auto', width: '100%' }}>
+          {provider_data && (
+            <Table compact striped>
+              <Table.Header>
+                {Object.keys(provider_data).map(k => (
+                  <Table.HeaderCell key={k}>{k}</Table.HeaderCell>
                 ))}
-            </Table.Body>
-          </Table>
-        )}
+              </Table.Header>
+              <Table.Body>
+                {Array(row_size)
+                  .fill()
+                  .map((_, i) => (
+                    <Table.Row>
+                      {Object.keys(provider_data).map(k => (
+                        <Table.Cell key={`${i}-${k}`}>
+                          {provider_data[k][i]}
+                        </Table.Cell>
+                      ))}
+                    </Table.Row>
+                  ))}
+              </Table.Body>
+            </Table>
+          )}
+        </div>
       </div>
     );
   }
