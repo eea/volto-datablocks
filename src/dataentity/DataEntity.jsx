3042a77128b1ebce43ea515b9dd40a0338d4acd5
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-01-02 16:08:38 +0200

    Implemented getValue method

--- src/dataentity/DataEntity.jsx
+++ src/dataentity/DataEntity.jsx
@@ -7,6 +7,40 @@ import { getDataFromProvider } from '../actions';
 import 'draft-js-focus-plugin/lib/plugin.css';
 import './styles.css';
 
+function getValue(data, column, filters) {
+  /*
+   * Data is an object like: {
+   * AV_P_SIZE: [501.78255, 849.335, 339.9433, 733.36331, 742.50659]
+   * COUNTRY: ["Albania", "Austria", "Belgium", ]
+   *
+   * Filters is an array of objects like:
+   * {
+   * i: "COUNTRY"
+   * o: "plone.app.querystring.operation.selection.any"
+   * v: ["BG"] }
+   */
+
+  // TODO: we implement now a very simplistic filtering, with only one type of
+  // filter and only one filter is taken into consideration
+
+  if (!data) return '';
+
+  const filter = filters[0];
+  const { i: index, v: values } = filter; // o: op,
+
+  if (!values || values.length === 0) return '';
+
+  // asuming that op is "plone.app.querystring.operation.selection.any"
+  const value = values[0];
+  const pos = data[index].indexOf(value);
+
+  if (pos === -1) {
+    console.warn(`No value found in data for "${value}" in column "${index}"`);
+    return '';
+  }
+  return data[column][pos];
+}
+
 const propTypes = {
   blockProps: PropTypes.object.isRequired,
   className: PropTypes.string,
@@ -30,12 +64,13 @@ class DataEntity extends Component {
     const { blockProps } = this.props;
     console.log('rendering dataentity', this.props);
 
-    return (
-      <span className="inline-data-entity">
-        <div class="inline-data-entity-text">{blockProps.url}</div>
-        <div class="inline-data-entity-text">{blockProps.column}</div>
-      </span>
+    const value = getValue(
+      this.props.provider_data,
+      blockProps.column,
+      this.props.content.data_query,
     );
+
+    return <span className="inline-data-entity">{value}</span>;
   }
 }
 
@@ -61,6 +96,7 @@ function getProviderData(state, props) {
 export default connect(
   (state, props) => ({
     provider_data: getProviderData(state, props),
+    content: state.content.data,
   }),
   {
     getDataFromProvider,
