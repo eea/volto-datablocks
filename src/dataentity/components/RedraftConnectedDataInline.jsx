87d6cc2f87dc235949003caa474e5e65985bdacf
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-01-10 15:00:05 +0200

    Hack inline data entity rendering

--- src/dataentity/components/RedraftConnectedDataInline.jsx
+++ src/dataentity/components/RedraftConnectedDataInline.jsx
@@ -5,35 +5,42 @@
 import React from 'react';
 import DataConnectedValue from 'volto-datablocks/DataConnectedValue';
 
-const RedraftConnectedInlineDataEntity = props => {
-  // console.log('redraft props', props);
+/*
+ * Recursively replace all text nodes in children with DataConnectedValues
+ * This is a hack, might not work in future React. Fingers cross
+ */
+function insertConnectedData(children, url, column) {
+  children.forEach((child, index) => {
+    if (typeof child === 'string') {
+      children[index] = (
+        <DataConnectedValue url={url} column={column} key={index} />
+      );
+    } else {
+      const innerChildren = child.props?.children || [];
+      innerChildren.forEach((c, i) => {
+        if (typeof c === 'string') {
+          innerChildren[i] = (
+            <DataConnectedValue url={url} column={column} key={index} />
+          );
+        } else {
+          insertConnectedData(child.props?.children || [], url, column);
+        }
+      });
+    }
+  });
 
+  return children;
+}
+
+const RedraftConnectedInlineDataEntity = props => {
   const {
     children,
     entityData: { column, url },
   } = props;
 
-  console.log('children', children);
   return url && column ? (
     <span className="inline-data-entity">
-      {children.map((child, index) => {
-        if (!child) return '';
-        // Note: this is a hack, it might not work in future react
-        if (child.props) {
-          child.props.children[0] = (
-            <DataConnectedValue url={url} column={column} key={index} />
-          );
-          if (child.props.children.length > 1) {
-            const range = new Array(child.props.children.length).fill(true);
-            range.forEach((_, i) => {
-              child.props.children[i + 1] = '';
-            });
-          }
-          return child;
-        } else {
-          return <DataConnectedValue url={url} column={column} key={index} />;
-        }
-      })}
+      {insertConnectedData(children, url, column)}
     </span>
   ) : (
     <span className="ui tag label red">{children}</span>
