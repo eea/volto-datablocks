8cff94ce6b9d731574e9cb209484f9332deff207
Mihai Macaneata <mihai.macaneata@eaudeweb.ro>
2020-01-28 15:47:03 +0200

    remove grid from sources

--- src/theme/Blocks/SourceView.jsx
+++ src/theme/Blocks/SourceView.jsx
@@ -23,40 +23,33 @@ const SourceView = ({
       )}
 
       <div className="sources">
-        <Grid columns={2} stretched>
-          <Grid.Row>
-            <Grid.Column width={10}>
-              <span className="discreet">
-                {initialSource || (multipleSources && multipleSources.length)
-                  ? multipleSources && multipleSources.length
-                    ? 'Sources: '
-                    : 'Source: '
-                  : ''}
-              </span>
+        <span className="discreet">
+          {initialSource || (multipleSources && multipleSources.length)
+            ? multipleSources && multipleSources.length
+              ? 'Sources: '
+              : 'Source: '
+            : ''}
+        </span>
+        <a
+          className="discreet block_source"
+          href={initialSourceLink}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {initialSource}
+        </a>
+        {multipleSources && multipleSources.length
+          ? multipleSources.map(item => (
               <a
                 className="discreet block_source"
-                href={initialSourceLink}
+                href={item.chart_source_link}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                {initialSource}
+                {item.chart_source}
               </a>
-              {multipleSources && multipleSources.length
-                ? multipleSources.map(item => (
-                    <a
-                      className="discreet block_source"
-                      href={item.chart_source_link}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {item.chart_source}
-                    </a>
-                  ))
-                : ''}
-            </Grid.Column>
-            <Grid.Column width={2} align="right" />
-          </Grid.Row>
-        </Grid>
+            ))
+          : ''}
       </div>
     </React.Fragment>
   );
