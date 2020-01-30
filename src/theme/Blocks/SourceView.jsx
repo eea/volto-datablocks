92c967d9ef4a3fb51e313b9dddcf79a11308741d
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-01-30 13:39:15 +0200

    Remove data download button

--- src/theme/Blocks/SourceView.jsx
+++ src/theme/Blocks/SourceView.jsx
@@ -4,55 +4,55 @@ import { Icon as VoltoIcon } from '@plone/volto/components';
 import { Grid } from 'semantic-ui-react';
 import { settings } from '~/config';
 
+//  <React.Fragment>
+//    {providerUrl && (
+//      <a
+//        href={`${settings.apiPath}${providerUrl}/@@download`}
+//        className="discreet download-button"
+//        title="Download data"
+//      >
+//        <VoltoIcon name={downloadSVG} size="20" />
+//      </a>
+//    )}
+
+//    <div className="sources">
+//      <span className="discreet">
+//        {initialSource || (multipleSources && multipleSources.length)
+//          ? multipleSources && multipleSources.length
+//            ? 'Sources: '
+//            : 'Source: '
+//          : ''}
+//      </span>
+//      <a
+//        className="discreet block_source"
+//        href={initialSourceLink}
+//        target="_blank"
+//        rel="noopener noreferrer"
+//      >
+//        {initialSource}
+//      </a>
+//      {multipleSources && multipleSources.length
+//        ? multipleSources.map(item => (
+//            <a
+//              className="discreet block_source"
+//              href={item.chart_source_link}
+//              target="_blank"
+//              rel="noopener noreferrer"
+//            >
+//              {item.chart_source}
+//            </a>
+//          ))
+//        : ''}
+//    </div>
+//  </React.Fragment>
+
 const SourceView = ({
   initialSource,
   initialSourceLink,
   multipleSources,
   providerUrl,
 }) => {
-  return (
-    <React.Fragment>
-      {providerUrl && (
-        <a
-          href={`${settings.apiPath}${providerUrl}/@@download`}
-          className="discreet download-button"
-          title="Download data"
-        >
-          <VoltoIcon name={downloadSVG} size="20" />
-        </a>
-      )}
-
-      <div className="sources">
-        <span className="discreet">
-          {initialSource || (multipleSources && multipleSources.length)
-            ? multipleSources && multipleSources.length
-              ? 'Sources: '
-              : 'Source: '
-            : ''}
-        </span>
-        <a
-          className="discreet block_source"
-          href={initialSourceLink}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {initialSource}
-        </a>
-        {multipleSources && multipleSources.length
-          ? multipleSources.map(item => (
-              <a
-                className="discreet block_source"
-                href={item.chart_source_link}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {item.chart_source}
-              </a>
-            ))
-          : ''}
-      </div>
-    </React.Fragment>
-  );
+  return '';
 };
 
 export default SourceView;
