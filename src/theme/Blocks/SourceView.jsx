c2dbb8a42a3e654ec6efec89156f46227b09176b
Mihai Macaneata <mihai.macaneata@eaudeweb.ro>
2020-01-30 17:01:30 +0200

    fix source view

--- src/theme/Blocks/SourceView.jsx
+++ src/theme/Blocks/SourceView.jsx
@@ -1,50 +1,8 @@
 import React from 'react';
-import downloadSVG from '@plone/volto/icons/download.svg';
-import { Icon as VoltoIcon } from '@plone/volto/components';
-import { Grid } from 'semantic-ui-react';
-import { settings } from '~/config';
-
-//  <React.Fragment>
-//    {providerUrl && (
-//      <a
-//        href={`${settings.apiPath}${providerUrl}/@@download`}
-//        className="discreet download-button"
-//        title="Download data"
-//      >
-//        <VoltoIcon name={downloadSVG} size="20" />
-//      </a>
-//    )}
-
-//    <div className="sources">
-//      <span className="discreet">
-//        {initialSource || (multipleSources && multipleSources.length)
-//          ? multipleSources && multipleSources.length
-//            ? 'Sources: '
-//            : 'Source: '
-//          : ''}
-//      </span>
-//      <a
-//        className="discreet block_source"
-//        href={initialSourceLink}
-//        target="_blank"
-//        rel="noopener noreferrer"
-//      >
-//        {initialSource}
-//      </a>
-//      {multipleSources && multipleSources.length
-//        ? multipleSources.map(item => (
-//            <a
-//              className="discreet block_source"
-//              href={item.chart_source_link}
-//              target="_blank"
-//              rel="noopener noreferrer"
-//            >
-//              {item.chart_source}
-//            </a>
-//          ))
-//        : ''}
-//    </div>
-//  </React.Fragment>
+// import downloadSVG from '@plone/volto/icons/download.svg';
+// import { Icon as VoltoIcon } from '@plone/volto/components';
+// import { Grid } from 'semantic-ui-react';
+// import { settings } from '~/config';
 
 const SourceView = ({
   initialSource,
@@ -52,7 +10,49 @@ const SourceView = ({
   multipleSources,
   providerUrl,
 }) => {
-  return '';
+  return (
+    <React.Fragment>
+      {/* {providerUrl && (
+        <a
+          href={`${settings.apiPath}${providerUrl}/@@download`}
+          className="discreet download-button"
+          title="Download data"
+        >
+          <VoltoIcon name={downloadSVG} size="20" />
+        </a>
+      )} */}
+
+      <div className="sources">
+        <span className="discreet">
+          {initialSource || (multipleSources && multipleSources.length)
+            ? multipleSources && multipleSources.length
+              ? 'Sources: '
+              : 'Source: '
+            : ''}
+        </span>
+        <a
+          className="discreet block_source"
+          href={initialSourceLink}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {initialSource}
+        </a>
+        {multipleSources && multipleSources.length
+          ? multipleSources.map(item => (
+              <a
+                className="discreet block_source"
+                href={item.chart_source_link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {item.chart_source}
+              </a>
+            ))
+          : ''}
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default SourceView;
