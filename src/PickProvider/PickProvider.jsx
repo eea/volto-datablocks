809817f98beeea95da07b47d415c0519fadf6b54
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-01-31 23:51:45 +0200

    Simplify PickProvider

--- src/PickProvider/PickProvider.jsx
+++ src/PickProvider/PickProvider.jsx
@@ -1,46 +1,11 @@
-import { find } from 'lodash';
+import PickObject from 'volto-addons/PickObject';
 import React, { Component } from 'react';
 import { addAppURL } from '@plone/volto/helpers';
 import { connect } from 'react-redux';
-import { DATA_PROVIDER_TYPES } from 'volto-datablocks/constants';
 import { getDataFromProvider } from 'volto-datablocks/actions';
-import { searchContent } from '@plone/volto/actions';
-import { Button, Grid } from 'semantic-ui-react';
-import Select from 'react-select';
-import { Link } from 'react-router-dom';
-import {
-  Option,
-  DropdownIndicator,
-  selectTheme,
-  customSelectStyles,
-} from '@plone/volto/components/manage/Widgets/SelectStyling';
-import {
-  getBoolean,
-  getVocabFromHint,
-  getVocabFromField,
-  getVocabFromItems,
-} from '@plone/volto/helpers';
-
-function getDefaultValues(choices, value) {
-  if (value && choices.length > 0) {
-    const found = find(choices, o => o.value === value);
-    return found;
-  } else {
-    return {};
-  }
-}
 
 class PickProvider extends Component {
   componentDidMount() {
-    // TODO: this needs to use a subrequest
-    this.props.searchContent(
-      '',
-      {
-        object_provides: DATA_PROVIDER_TYPES,
-        b_size: 10000,
-      },
-      'getProviders',
-    );
     if (this.props.value) {
       this.props.getDataFromProvider(this.props.value);
     }
@@ -66,65 +31,13 @@ class PickProvider extends Component {
   };
 
   render() {
-    const selectProviders = this.props.providers.map(el => {
-      return [el['@id'], el.title];
-    });
-
-    const choices = [
-      ...selectProviders.map(option => ({
-        value: option[0],
-        label: option[1],
-        selected: option[0] === this.props.value,
-      })),
-      {
-        label: 'No value',
-        value: 'no-value',
-      },
-    ];
-
     return (
-      <div className="inline field">
-        <Grid>
-          <Grid.Row stretched>
-            <Grid.Column width={4}>
-              <div className="wrapper">
-                {this.props.value ? (
-                  <Link to={this.props.value}>Provider:</Link>
-                ) : (
-                  <label htmlFor={`field-${this.props.id}`}>Provider:</label>
-                )}
-              </div>
-            </Grid.Column>
-            <Grid.Column width={8}>
-              <div>
-                <Select
-                  id={`field-${this.props.id}`}
-                  name={this.props.id}
-                  className="react-select-container"
-                  classNamePrefix="react-select"
-                  options={choices}
-                  styles={customSelectStyles}
-                  theme={selectTheme}
-                  components={{ DropdownIndicator, Option }}
-                  value={getDefaultValues(choices, this.props.value)}
-                  onChange={data =>
-                    this.props.onChange(
-                      data.value === 'no-value' ? undefined : data.value,
-                    )
-                  }
-                />
-                {this.props.showReload ? (
-                  <Button onClick={this.refresh} disabled={!this.props.value}>
-                    Reload data
-                  </Button>
-                ) : (
-                  ''
-                )}
-              </div>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </div>
+      <PickObject
+        id="provider"
+        title="Provider"
+        value={this.props.value}
+        onChange={this.props.onChange}
+      />
     );
   }
 }
@@ -147,9 +60,105 @@ export default connect(
     const providerData = getProviderData(state, props);
 
     return {
-      providers: state.search.subrequests?.getProviders?.items || [],
       providerData,
     };
   },
-  { searchContent, getDataFromProvider },
+  { getDataFromProvider },
 )(PickProvider);
+
+// TODO: this needs to use a subrequest
+// this.props.searchContent(
+//   '',
+//   {
+//     object_provides: DATA_PROVIDER_TYPES,
+//     b_size: 10000,
+//   },
+//   'getProviders',
+// );
+// import { DATA_PROVIDER_TYPES } from 'volto-datablocks/constants';
+// import { searchContent } from '@plone/volto/actions';
+//searchContent,
+// providers: state.search.subrequests?.getProviders?.items || [],
+// const selectProviders = this.props.providers.map(el => {
+//   return [el['@id'], el.title];
+// });
+//
+// const choices = [
+//   ...selectProviders.map(option => ({
+//     value: option[0],
+//     label: option[1],
+//     selected: option[0] === this.props.value,
+//   })),
+//   {
+//     label: 'No value',
+//     value: 'no-value',
+//   },
+// ];
+//    <div className="inline field">
+//      <Grid>
+//        <Grid.Row stretched>
+//          <Grid.Column width={4}>
+//            <div className="wrapper">
+//              {this.props.value ? (
+//                <Link to={this.props.value}>Provider:</Link>
+//              ) : (
+//                <label htmlFor={`field-${this.props.id}`}>Provider:</label>
+//              )}
+//            </div>
+//          </Grid.Column>
+//          <Grid.Column width={8}>
+//            <div>
+//              <Select
+//                id={`field-${this.props.id}`}
+//                name={this.props.id}
+//                className="react-select-container"
+//                classNamePrefix="react-select"
+//                options={choices}
+//                styles={customSelectStyles}
+//                theme={selectTheme}
+//                components={{ DropdownIndicator, Option }}
+//                value={getDefaultValues(choices, this.props.value)}
+//                onChange={data =>
+//                  this.props.onChange(
+//                    data.value === 'no-value' ? undefined : data.value,
+//                  )
+//                }
+//              />
+//              {this.props.showReload ? (
+//                <Button onClick={this.refresh} disabled={!this.props.value}>
+//                  Reload data
+//                </Button>
+//              ) : (
+//                ''
+//              )}
+//            </div>
+//          </Grid.Column>
+//        </Grid.Row>
+//      </Grid>
+//    </div>
+//  );
+// function getDefaultValues(choices, value) {
+//   if (value && choices.length > 0) {
+//     const found = find(choices, o => o.value === value);
+//     return found;
+//   } else {
+//     return {};
+//   }
+// }
+//
+// import { find } from 'lodash';
+// import { Button, Grid } from 'semantic-ui-react';
+// import Select from 'react-select';
+// import { Link } from 'react-router-dom';
+// import {
+//   Option,
+//   DropdownIndicator,
+//   selectTheme,
+//   customSelectStyles,
+// } from '@plone/volto/components/manage/Widgets/SelectStyling';
+// import {
+//   getBoolean,
+//   getVocabFromHint,
+//   getVocabFromField,
+//   getVocabFromItems,
+// } from '@plone/volto/helpers';
