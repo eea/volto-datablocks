37a5bec828f8754f5974a3cb88a143a06146299d
razvanMiu <miu.razvan28@gmail.com>
2020-12-15 13:49:04 +0200

    Added Jenkinsfile. Added cypress

---
+++ cypress/helpers/index.js
@@ -0,0 +1,2 @@
+export { openSidebar, closeSidebar, openSidebarTab } from './sidebar';
+export { changePageTitle, addBlock, selectBlock } from './blocks';
---
+++ cypress/integration/block-basics.js
@@ -0,0 +1,44 @@
+import { setupBeforeEach, tearDownAfterEach } from '../support';
+import { changePageTitle, addBlock } from '../helpers';
+import { openSidebarTab } from '../helpers';
+
+const group = {
+  title: 'Data blocks',
+  id: 'data_blocks',
+};
+
+const blocksIds = [
+  'data_connected_block',
+  'auto_select_parameter',
+  'discodata_connector_block',
+  'discodata_sql_builder',
+  'discodata_table_block',
+  'discodata_components_block',
+];
+
+describe('Blocks Tests', () => {
+  beforeEach(setupBeforeEach);
+  afterEach(tearDownAfterEach);
+
+  it('Add Blocks: Empty', () => {
+    changePageTitle('Volto datablocks tests');
+    blocksIds.forEach((id) => {
+      addBlock(group.title, group.id, id);
+    });
+  });
+});
+
+describe('Discodata sql builder tests', () => {
+  beforeEach(setupBeforeEach);
+  afterEach(tearDownAfterEach);
+
+  it('Add Block: Empty', () => {
+    changePageTitle('Volto datablocks tests');
+    addBlock(group.title, group.id, 'discodata_sql_builder');
+    openSidebarTab('Block');
+    cy.get('input#field-field-widget-provider_url').should(
+      'have.value',
+      'https://discodata.eea.europa.eu/sql',
+    );
+  });
+});
