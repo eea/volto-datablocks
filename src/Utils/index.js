27ec3dc0c2efd072690f6fcfdf617bc13e905643
razvanMiu <miu.razvan28@gmail.com>
2021-01-14 10:47:10 +0200

    Added DataConnectedValue

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -1,5 +1,6 @@
 export DB from './DataBase/DB';
 export PickProvider from './PickProvider/PickProvider';
+export DataConnectedValue from './DataConnectedValue/DataConnectedValue';
 
 export AddLinkForm from './AddLinkForm';
 export RenderFields from './RenderFields';
