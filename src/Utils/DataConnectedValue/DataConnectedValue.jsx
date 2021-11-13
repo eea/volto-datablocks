86a01a69ef8880311864846532e8d56c9e4fe89a
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-11-13 22:05:17 +0200

    Remove unused imports

--- src/Utils/DataConnectedValue/DataConnectedValue.jsx
+++ src/Utils/DataConnectedValue/DataConnectedValue.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
-import { getBaseUrl, flattenToAppURL } from '@plone/volto/helpers';
+// import { getBaseUrl, flattenToAppURL } from '@plone/volto/helpers';
 
 import { getDataFromProvider } from '../../actions';
 import {
