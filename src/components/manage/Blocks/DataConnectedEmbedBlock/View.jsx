ed605463250aaa8f0468b052afedff16e2dfaea3
nileshgulia1 <nileshgulia@gmail.com>
2022-04-28 15:42:39 +0530

    safari: don't use lookbehind

--- src/components/manage/Blocks/DataConnectedEmbedBlock/View.jsx
+++ src/components/manage/Blocks/DataConnectedEmbedBlock/View.jsx
@@ -21,11 +21,15 @@ const messages = defineMessages({
 const getFilteredURL = (url, connected_data_parameters = []) => {
   if (!connected_data_parameters?.length) return url;
   let decodedURL = decodeURIComponent(url);
-  const queries = decodedURL.match(/(?<=(<<))(.*?)(?=(>>))/g);
-  if (!queries?.length) return url;
+  const queries = decodedURL.match(/(<<)(.*?)*>>$/g); //safari: don't use lookbehind
+  const filteredQueries = queries.map((query) =>
+    query.replace('<<', '').replace('>>', ''),
+  );
+
+  if (!filteredQueries?.length) return url;
   const keys = connected_data_parameters.map((param) => param.i);
-  for (let poz in queries) {
-    const key = queries[poz];
+  for (let poz in filteredQueries) {
+    const key = filteredQueries[poz];
     const paramPoz = keys.indexOf(key);
     if (paramPoz > -1) {
       decodedURL = decodedURL.replace(
