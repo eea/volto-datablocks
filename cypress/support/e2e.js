48721ae1a1d444e3af7c183597b5807bd2ee409e
Claudia Ifrim <ifrim.claudia@gmail.com>
2023-05-10 00:07:37 +0300

    cypress config - Fail fast

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,6 +18,9 @@ import './commands';
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
+// Fail Fast
+import 'cypress-fail-fast';
+
 //Generate code-coverage
 import '@cypress/code-coverage/support';
 
