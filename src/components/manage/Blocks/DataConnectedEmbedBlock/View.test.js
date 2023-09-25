5a9fd5f783a5a44b4b121383e860d2422cdbcbc3
andreiggr <andrei.ggr@gmail.com>
2023-09-25 12:35:13 +0300

    chore: tests dep

--- src/components/manage/Blocks/DataConnectedEmbedBlock/View.test.js
+++ src/components/manage/Blocks/DataConnectedEmbedBlock/View.test.js
@@ -3,6 +3,7 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import { IntlProvider } from 'react-intl';
+import '@testing-library/jest-dom/extend-expect';
 
 jest.mock('@eeacms/volto-embed/PrivacyProtection/PrivacyProtection', () => {
   return jest.fn(({ children }) => <div>{children}</div>);
