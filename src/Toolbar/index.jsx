ff765d3ee15a715df1d6b3d79a054d31188841af
Miu Razvan <miu.razvan28@gmail.com>
2023-11-01 13:56:23 +0200

    moved toolbar in volto-embead

--- src/Toolbar/index.jsx
+++
@@ -1,5 +0,0 @@
-export { default as FigureNote } from './FigureNote';
-export { default as Sources } from './Sources';
-export { default as MoreInfo } from './MoreInfo';
-export { default as Share } from './Share';
-export { default as Enlarge } from './Enlarge';
