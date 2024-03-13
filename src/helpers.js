99467504926aab8b2e8f0af3a86e5cfa64a0bf0e
Razvan <miu.razvan28@gmail.com>
2024-03-13 15:28:19 +0200

    fix tests

--- src/helpers.js
+++ src/helpers.js
@@ -125,7 +125,7 @@ export function updateChartDataFromProvider(chartData, providerData) {
           trace[traceKey],
         );
         newTrace[originalColumnName] =
-          providerData[providerColumnName] || trace[originalColumnName] || [];
+          providerData[providerColumnName] || trace[originalColumnName];
       }
     });
 
