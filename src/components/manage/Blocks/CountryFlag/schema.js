c68782fefb6d4880e9efca09e3c3b9ca254e3613
laszlocseh <csehlaszlo91@gmail.com>
2024-05-15 11:23:11 +0300

    chore: update countryFlag schema descriptions

--- src/components/manage/Blocks/CountryFlag/schema.js
+++ src/components/manage/Blocks/CountryFlag/schema.js
@@ -23,6 +23,7 @@ const CountryFlagSchema = () => {
     properties: {
       country_name: {
         title: 'Country name',
+        description: 'Select the country to be displayed.',
         choices: [],
       },
       render_as: {
@@ -30,14 +31,20 @@ const CountryFlagSchema = () => {
       },
       show_name: {
         title: 'Show country name',
+        description:
+          'Enable it to display the name of the selected country. Disable it to display the title of the page.',
         type: 'boolean',
       },
       show_flag: {
         title: 'Show country flag',
+        description:
+          'Enable it to display the flag of the selected country. Disable it to display the preview image of the current page. If the page has no preview image set, it will display nothing.',
         type: 'boolean',
       },
       show_dropdown: {
         title: 'Show dropdown',
+        description:
+          'Enable the dropdown select to navigate to other pages. In the "Dropdown items" section you can set the criterias to filter the items displayed in the dropdown. If no criterias are set, the siblings of the current page are displayed in the dropdown.',
         type: 'boolean',
       },
       querystring: {
